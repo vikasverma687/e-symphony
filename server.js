cba9c7185c958cfc5c750f8a39e69e8edb47207c
vikasverma687 <vikasverma1682vert@gmail.com>
2024-11-20 23:40:04 +0530

    project commit

---
+++ server.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var port = 3000; // Port to run the server
+var host = 'localhost';
+
+http.createServer(function (req, res) {
+    var filePath = '.' + req.url;
+
+    // Default to serving index.html
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm',
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end('Sorry, there was an error: ' + error.code + ' ..\n');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+}).listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+});
